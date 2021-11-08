58827aab1b64c9393aaa20df11c09536b11b0e67
ETNJTOTG <cwq1102300125@yeah.net>
2021-11-08 17:16:50 +0800

    feat(test1): 新增test1

---
+++ test1/src/main/java/com/cwq/Test1Application.java
@@ -0,0 +1,15 @@
+package com.cwq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wenqing.chen  2021/11/08 17:08
+ **/
+@SpringBootApplication
+public class Test1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test1Application.class, args);
+    }
+}
